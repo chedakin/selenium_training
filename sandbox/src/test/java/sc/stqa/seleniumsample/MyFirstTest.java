067c6ce7f80a21a5b1d45ccf98a690a4f8a8bb5e
chedakin <101333091+chedakin@users.noreply.github.com>
2022-04-14 18:33:18 +0300

    Создан проэкт для изучения Selenium WebDriver

---
+++ sandbox/src/test/java/sc/stqa/seleniumsample/MyFirstTest.java
@@ -0,0 +1,44 @@
+package sc.stqa.seleniumsample;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        //don't need anymore since added into %PATH%
+        //System.setProperty("webdriver.gecko.driver","C:\\JavaTmp\\geckodriver.exe");
+        //System.setProperty("webdriver.chrome.driver","C:\\JavaTmp\\chromedriver.exe");
+        //System.setProperty("webdriver.edge.driver","C:\\JavaTmp\\msedgedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @AfterTest
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
